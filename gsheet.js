d8cf77c9e9ae311826f587eec49fbcabc3f8948d
surya-pmaurya <mauryasurya22@gmail.com>
2025-07-07 13:41:17 +0530

    uodated to responsiv for tab screen

--- gsheet.js
+++ gsheet.js
@@ -2,8 +2,36 @@ const scriptURL =
   "https://script.google.com/macros/s/AKfycbzQ5fIu0Z8BualyglVHdbpUykRKTVKgu-JzUdbm-R0_SlaTpqK7wjRjwCRrFdnC85S-1A/exec";
 const form = document.forms["submit-to-google-sheet"];
 const msg = document.getElementById("msg");
+const errorMsg = document.getElementById("emailError");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  // for email validation
+  const inputArea = document.getElementById("inputEmail");
+  const email = inputArea.value.trim();
+  errorMsg.textContent = "";
+  errorMsg.style.opacity = "0";
+  if (email === "") {
+    setTimeout(() => {
+      errorMsg.textContent = "Email is required.";
+      errorMsg.style.opacity = "1";
+    }, 100);
+    return;
+  }
+  if (
+    !(
+      email.endsWith("@gmail.com") ||
+      email.endsWith("@yahoo.com") ||
+      email.endsWith("@outlook.com")
+    )
+  ) {
+    setTimeout(() => {
+      errorMsg.textContent = "Please enter a valid Gmail address.";
+      errorMsg.style.opacity = "1";
+    }, 100);
+    return;
+  }
+
   msg.style.opacity = "0";
   setTimeout(() => {
     msg.style.color = "#ffc107"; // Yellow
--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-// About Section Skill Experience and Education
+//Role Heading Function
 
+const roles = [
+  "Back-End Developer",
+  "Front-End Developer",
+  "Software Developer",
+];
+const typingElement = document.getElementById("typing-role");
+let roleIndex = 0;
+let charIndex = 0;
+
+function typeRole() {
+  if (charIndex < roles[roleIndex].length) {
+    typingElement.innerHTML += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(typeRole, 100); // Typing speed
+  } else {
+    setTimeout(eraseRole, 2000); // Wait before erasing
+  }
+}
+
+function eraseRole() {
+  if (charIndex > 0) {
+    typingElement.innerHTML = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(eraseRole, 50); // Erase speed
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(typeRole, 1000); // Delay before typing next role
+  }
+}
+// Delay typing start (e.g., 1.5s after page load)
+setTimeout(typeRole, 1500);
+
+// About Section Skill Experience and Education
 let tablinks = document.getElementsByClassName("tab-links");
 let tabcontents = document.getElementsByClassName("tab-contents");
 function opentab(tabname) {
